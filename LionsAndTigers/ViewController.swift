2eb711ac04192494eb042d6389bd7c0f67e22697
Mauro Consiglio <Mauro@Mauro-Mac-Mini.local>
2014-11-13 20:50:53 +0100

    first commit

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var randomFactLabel: UILabel!
+    
+    var myTigers:[Tiger] = []
+    
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named:"BengalTiger.jpg")
+        
+        myTiger.age = myTiger.ageInTigerYearsFromAge(myTiger.age)
+        
+        myTiger.chuff()
+        myTiger.chuffANumberOfTimes(5, isLoud: true)
+        
+        self.myTigers.append(myTiger)
+        
+        println("My tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed) and its image is \(myTiger.image)")
+        
+        self.myImageView.image = myTiger.image
+        self.nameLabel.text = myTiger.name
+        self.ageLabel.text = "\(myTiger.age)"
+        self.breedLabel.text = myTiger.breed
+        self.randomFactLabel.text = myTiger.randomFact()
+        
+        var secondTiger = Tiger ()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        secondTiger.age = secondTiger.ageInTigerYearsFromAge(secondTiger.age)
+        secondTiger.chuff()
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        thirdTiger.age = thirdTiger.ageInTigerYearsFromAge(thirdTiger.age)
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
+        fourthTiger.age = fourthTiger.ageInTigerYearsFromAge(fourthTiger.age)
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        
+        myTiger.chuffANumberOfTimes(3)
+        secondTiger.chuffANumberOfTimes(2)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +80,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        var randomIndex:Int
+        
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        } while self.currentIndex == randomIndex
+        
+        self.currentIndex = randomIndex
+        
+        let tiger = self.myTigers[randomIndex]
+        
+//        myImageView.image = tiger.image
+//        nameLabel.text = tiger.name
+//        ageLabel.text = "\(tiger.age)"
+//        breedLabel.text = tiger.breed
+        
+        UIView.transitionWithView(self.view, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            
+            self.myImageView.image = tiger.image
+            self.nameLabel.text = tiger.name
+            self.ageLabel.text = "\(tiger.age)"
+            self.breedLabel.text = tiger.breed
+            self.randomFactLabel.text = tiger.randomFact()
+            
+            }, completion: {
+                (finished: Bool) -> () in
+        })
+    }
 
 }
 
